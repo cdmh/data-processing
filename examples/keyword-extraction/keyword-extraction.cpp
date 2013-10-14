30259d3e94e920c4fcffcf6721ad473e757d9c0c
Craig Henderson <cdm.henderson@gmail.com>
2013-10-14 09:09:22 +0100

    initial classifier class

--- examples/keyword-extraction/keyword-extraction.cpp
+++ examples/keyword-extraction/keyword-extraction.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include <locale>
 #include <map>
+#include <memory>
 #include "../../data-processing.h"
 
 namespace {     // anonymous namespace
@@ -64,7 +65,7 @@ bool const is_word_char(char ch)
     return (ch >= '0'  &&  ch <= '9')
        ||  (ch >= 'a'  &&  ch <= 'z')
        ||  (ch >= 'A'  &&  ch <= 'Z')
-       ||   ch == '-'  ||  ch == '_';
+       ||   ch == '-'  ||  ch == '_'  ||  ch == '\'';
 }
 
 template<typename It>
@@ -177,10 +178,106 @@ void create_word_list_and_map(
 #include "naive-bayes-classifier/src/ActionClassifier.h"
 #include "naive-bayes-classifier/src/Domain.h"
 
-void classify(BayesianClassifier &classifier, string_view const &title)
+/*
+  inspired by http://www.inf.ed.ac.uk/teaching/courses/inf2b/learnnotes/inf2b-learn-note07-2up.pdf
+*/
+class classifier
 {
-    classifier.calculateOutput({});
-}
+  public:
+    explicit classifier(cdmh::data_processing::dataset const &ds) : ds_(ds)
+    {
+        create_word_list_and_map(ds, 1, title_words, title_word_indices);
+        create_word_list_and_map(ds, 3, tag_words, tag_word_indices);
+
+        // create domains:
+        //     title_words.size() input domains
+        //     tag_words.size() output domains
+	    std::vector<Domain> domains;
+        for (size_t d=0; d<title_words.size() + tag_words.size(); ++d)
+	        domains.emplace_back(0.0f, 1.0f, 2);  // min, max, number of values
+        classifier_.reset(new BayesianClassifier(domains));
+    }
+
+    void train(
+        size_t training_rows_begin,
+        size_t training_rows_end)
+    {
+        std::vector<float> training;
+        training.resize(title_words.size() + tag_words.size(), 0.0);
+        for (size_t loop=training_rows_begin; loop<training_rows_end; ++loop)
+        {
+            // reset the vector to contain zeros
+            for (float &value : training)
+                value = 0.0;
+
+            auto string = ds_[loop][1].get<string_view>();
+            auto it  = string.begin();
+            auto ite = string.end();
+            while (it != ite)
+            {
+                string_view const word = next_word(it, ite);
+                if (word.length() > 0)
+                {
+                    auto it = title_word_indices.find(word);
+                    assert(it != title_word_indices.end());
+                    training[it->second] = 1.0;    // !!!binary title does/doesn't contain tag. Count might work better?
+                }
+            }
+            std::cout << "\n\n" << string;
+
+            string = ds_[loop][3].get<string_view>();
+            it  = string.begin();
+            ite = string.end();
+            while (it != ite)
+            {
+                string_view const word = next_word(it, ite);
+                if (word.length() > 0)
+                {
+                    auto it = tag_word_indices.find(word);
+                    assert(it != tag_word_indices.end());
+                    training[it->second + title_words.size()] = 1.0;    // !!!binary title does/doesn't contain tag. Count might work better?
+                }
+            }
+
+#if 0 && !defined(NDEBUG)
+            std::cout << "\n";
+            for (float &value : training)
+                std::cout << (int)value;
+#endif
+
+            std::cout << "\n";
+            for (size_t loop=0; loop<training.size(); ++loop)
+            {
+                if (training[loop])
+                {
+                    if (loop < title_words.size())
+                        std::cout << title_words[loop] << " ";
+                    else
+                        std::cout << "\n*** " << tag_words[loop - title_words.size()] << " ";
+                }
+            }
+            std::cout << "\n\n";
+            classifier_->addRawTrainingData(training);
+        }
+    }
+
+    void classify(string_view const &title)
+    {
+        classifier_->calculateOutput({});
+    }
+
+  private:
+    // Dataset format: id, title, body, tags
+    cdmh::data_processing::dataset const &ds_;
+
+    // train a bayesian classifier
+    std::unique_ptr<BayesianClassifier> classifier_;
+
+    std::vector<string_view>   title_words;
+    std::map<string_view, int> title_word_indices;
+    std::vector<string_view>   tag_words;
+    std::map<string_view, int> tag_word_indices;
+};
 
 int main(int argc, char const *argv[])
 {
@@ -196,13 +293,12 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-
     std::cout << "Loading file ...";
     cdmh::data_processing::dataset ds;
 #ifdef NDEBUG
     size_t num_rows = 0;
 #else
-    size_t num_rows = 5;
+    size_t num_rows = 1000;
 #endif
     ds.attach(mmf.get(), mmf.get() + mmf.size(), num_rows);
 
@@ -210,83 +306,15 @@ int main(int argc, char const *argv[])
     ds.write_column_info(std::cout);
     std::cout << "\n";
 
-    // id, title, body, tags
+    // use two thirds for training and a third for testing
+    size_t const training_rows_begin = 0;
+    size_t const training_rows_end   = size_t(ds.rows() * 0.666667);
+    size_t const test_rows_begin     = training_rows_end + 1;
+    size_t const test_rows_end       = ds.rows();
 
-    std::vector<string_view>   title_words;
-    std::map<string_view, int> title_word_indices;
-    create_word_list_and_map(ds, 1, title_words, title_word_indices);
-
-    std::vector<string_view>   tag_words;
-    std::map<string_view, int> tag_word_indices;
-    create_word_list_and_map(ds, 3, tag_words, tag_word_indices);
-
-    /*
-     inspired by http://www.inf.ed.ac.uk/teaching/courses/inf2b/learnnotes/inf2b-learn-note07-2up.pdf
-    */
-
-    // create domains:
-    //     title_words.size() input domains
-    //     tag_words.size() output domains
-	std::vector<Domain> domains;
-    for (size_t d=0; d<title_words.size() + tag_words.size(); ++d)
-	    domains.emplace_back(0.0f, 1.0f, 2);  // min, max, number of values
-
-    // train a bayesian classifier
-    BayesianClassifier classifier(domains);
-    std::vector<float> training;
-    training.resize(title_words.size() + tag_words.size(), 0.0);
-    for (size_t loop=0; loop<ds.rows(); ++loop)
-    {
-        // reset the vector to contain zeros
-        for (float &value : training)
-            value = 0.0;
-
-        auto string = ds[loop][1].get<string_view>();
-        auto it  = string.begin();
-        auto ite = string.end();
-        while (it != ite)
-        {
-            string_view const word = next_word(it, ite);
-            if (word.length() > 0)
-            {
-                auto index = title_word_indices[word];
-                training[index] = 1.0;                                          // !!!binary title does/doesn't contain tag. Count might work better?
-            }
-        }
-        std::cout << "\n\n" << string;
-
-        string = ds[loop][3].get<string_view>();
-        it  = string.begin();
-        ite = string.end();
-        while (it != ite)
-        {
-            string_view const word = next_word(it, ite);
-            if (word.length() > 0)
-                training[title_words.size() + tag_word_indices[word]] = 1.0;    // !!!binary title does/doesn't contain tag. Count might work better?
-        }
-
-#ifndef NDEBUG
-        std::cout << "\n";
-        for (float &value : training)
-            std::cout << (int)value;
-#endif
-
-        std::cout << "\n";
-        for (size_t loop=0; loop<training.size(); ++loop)
-        {
-            if (training[loop])
-            {
-                if (loop < title_words.size())
-                    std::cout << title_words[loop] << " ";
-                else
-                    std::cout << "\n*** " << tag_words[loop - title_words.size()] << " ";
-            }
-        }
-        std::cout << "\n\n";
-        classifier.addRawTrainingData(training);
-    }
-
-    classify(classifier, "Getting rid of site-specific hotkeys");
-    classify(classifier, "Remove old vCenter servers from VMWare vSphere Client login window");
+    classifier bayesian(ds);
+    bayesian.train(training_rows_begin, training_rows_end);
+    bayesian.classify("Getting rid of site-specific hotkeys");
+    bayesian.classify("Remove old vCenter servers from VMWare vSphere Client login window");
 	return 0;
 }

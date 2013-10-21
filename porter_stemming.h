5851d53b77b21a2634bd12342b501e791014fd3e
Craig Henderson <cdm.henderson@gmail.com>
2013-10-21 11:53:37 +0100

    Added tests for stemming algorithm

---
+++ porter_stemming.h
@@ -0,0 +1,32 @@
+// An implementation of the Porter Stemming Algorithm
+// For a description, see http://tartarus.org/martin/PorterStemmer/
+// 
+// Copyright (c) 2013 Craig Henderson
+// Part of the Data Processing Library
+// https://github.com/cdmh/dataproc
+
+#include <string>
+
+namespace cdmh {
+namespace data_processing {
+
+template<typename It>
+std::string porter_stemmer(It it, It ite)
+{
+    return std::string(&*it, &*ite);
+}
+
+std::string porter_stemmer(char const *string)
+{
+    auto stem = porter_stemmer(string, string + strlen(string));
+    return std::string(stem.begin(), stem.end());
+}
+
+std::string porter_stemmer(std::string const &string)
+{
+    auto stem = porter_stemmer(string.cbegin(), string.cend());
+    return std::string(stem.begin(), stem.end());
+}
+
+}   // namespace data_processing
+}   // namespace cdmh

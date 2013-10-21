01c45aaf847572eb5d6c0aa77263c22bc1c6280e
Craig Henderson <cdm.henderson@gmail.com>
2013-10-21 11:49:13 +0100

    restructed files for sharing

--- data-processing.h
+++ data-processing.h
@@ -19,29 +19,6 @@
 
 namespace cdmh {
 
-#if WIN32 && !defined(strncasecmp)
-inline int __cdecl strncasecmp(const char * first, const char * last, size_t count)
-{
-    if (!count)
-        return 0;
-
-    int f = 0;
-    int l = 0;
-
-    do
-    {
-        if (((f = *(first++)) >= 'A')  &&  (f <= 'Z'))
-            f -= 'A' - 'a';
-
-        if (((l = *(last++)) >= 'A')  &&  (l <= 'Z'))
-            l -= 'A' - 'a';
-    }
-    while (--count  &&  f  &&  (f == l));
-
-    return (f - l);
-}
-#endif
-
 namespace data_processing {
 
 template<typename T, typename U>
@@ -61,73 +38,6 @@ std::vector<T> split_string(U const &string, char const delim)
     return result;
 }
 
-// a string type of a string of characters
-// represented by a pair of iterators
-class string_view
-{
-  public:
-    string_view(char const *begin)
-      : begin_(begin), end_(begin + strlen(begin))
-    { }
-
-    string_view(char const *begin,char const *end)
-      : begin_(begin),end_(end)
-    { }
-
-    char const *begin() const { return begin_; }
-    char const *end()   const { return end_; }
-
-    size_t const length() const
-    {
-        return std::distance(begin_, end_);
-    }
-
-  private:
-    char const *begin_;
-    char const *end_;
-};
-
-inline
-bool const operator==(string_view const &first, char const *second)
-{
-    auto const len1 = first.length();
-    auto const len2 = strlen(second);
-    if (len1 != len2)
-        return false;
-
-    return (strncasecmp(first.begin(), second, len1) == 0);
-}
-
-inline
-bool const operator<(string_view const &first, string_view const &second)
-{
-    auto const len1 = first.length();
-    auto const len2 = second.length();
-    if (len1 < len2)
-    {
-        auto cmp = strncasecmp(first.begin(), second.begin(), len1);
-        return (cmp <= 0);
-    }
-    else if (len1 > len2)
-    {
-        auto cmp = strncasecmp(first.begin(), second.begin(), len2);
-        return (cmp < 0);
-    }
-
-    return strncasecmp(first.begin(), second.begin(), len1) < 0;
-}
-
-bool const operator==(string_view const &first, string_view const &second);
-
-template<typename E, typename T>
-inline
-std::basic_ostream<E, T> &operator<<(std::basic_ostream<E, T> &o, string_view const &str)
-{
-    std::copy(str.begin(), str.end(), std::ostream_iterator<char>(o));
-    return o;
-}
-
-
 }   // namespace data_processing
 }   // namespace cdmh
 
@@ -142,6 +52,7 @@ static cdmh::data_processing::type_mask_t const null_type    = 1 << 3;
 }   // anonymous namespace
 
 // project header files
+#include "string_view.h"
 #include "dataset.h"
 #include "mapped_csv.h"
 #include "maths.h"
---
+++ string_view.h
@@ -0,0 +1,105 @@
+// Copyright (c) 2013 Craig Henderson
+// Part of the Data Processing Library
+// https://github.com/cdmh/dataproc
+
+#pragma once
+
+#include <cstring>
+#include <algorithm>
+
+namespace cdmh {
+
+#if WIN32 && !defined(strncasecmp)
+inline int __cdecl strncasecmp(const char * first, const char * last, size_t count)
+{
+    if (!count)
+        return 0;
+
+    int f = 0;
+    int l = 0;
+
+    do
+    {
+        if (((f = *(first++)) >= 'A')  &&  (f <= 'Z'))
+            f -= 'A' - 'a';
+
+        if (((l = *(last++)) >= 'A')  &&  (l <= 'Z'))
+            l -= 'A' - 'a';
+    }
+    while (--count  &&  f  &&  (f == l));
+
+    return (f - l);
+}
+#endif
+
+namespace data_processing {
+
+// a string type of a string of characters
+// represented by a pair of iterators
+class string_view
+{
+  public:
+    string_view(char const *begin)
+      : begin_(begin), end_(begin + strlen(begin))
+    { }
+
+    string_view(char const *begin,char const *end)
+      : begin_(begin),end_(end)
+    { }
+
+    char const *begin() const { return begin_; }
+    char const *end()   const { return end_; }
+
+    size_t const length() const
+    {
+        return std::distance(begin_, end_);
+    }
+
+  private:
+    char const *begin_;
+    char const *end_;
+};
+
+inline
+bool const operator==(string_view const &first, char const *second)
+{
+    auto const len1 = first.length();
+    auto const len2 = strlen(second);
+    if (len1 != len2)
+        return false;
+
+    return (strncasecmp(first.begin(), second, len1) == 0);
+}
+
+inline
+bool const operator<(string_view const &first, string_view const &second)
+{
+    auto const len1 = first.length();
+    auto const len2 = second.length();
+    if (len1 < len2)
+    {
+        auto cmp = strncasecmp(first.begin(), second.begin(), len1);
+        return (cmp <= 0);
+    }
+    else if (len1 > len2)
+    {
+        auto cmp = strncasecmp(first.begin(), second.begin(), len2);
+        return (cmp < 0);
+    }
+
+    return strncasecmp(first.begin(), second.begin(), len1) < 0;
+}
+
+bool const operator==(string_view const &first, string_view const &second);
+
+template<typename E, typename T>
+inline
+std::basic_ostream<E, T> &operator<<(std::basic_ostream<E, T> &o, string_view const &str)
+{
+    std::copy(str.begin(), str.end(), std::ostream_iterator<char>(o));
+    return o;
+}
+
+
+}   // namespace data_processing
+}   // namespace cdmh
